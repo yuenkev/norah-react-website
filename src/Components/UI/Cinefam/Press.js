b925c692f8c8023cb30658aee297e2bd3c9ead2b
Kevin Yuen <90118768+yuenkev@users.noreply.github.com>
2023-09-10 20:19:13 -0400

    Third Commit: Added Press Hits
    -Added Press Hits to the "cinefam woc database" page

--- src/Components/UI/Cinefam/Press.js
+++ src/Components/UI/Cinefam/Press.js
@@ -1,10 +1,26 @@
 import React from "react";
 import classes from "./Press.module.css";
+import { hits } from "./PressData";
 
 const Press = () => {
   return (
     <div className={classes.press}>
-        <h4>Press Hits</h4>
+      <h4>Press Hits</h4>
+      <div className={classes.hits}>
+        {hits.map((hits) => {
+          return (
+            <div className={classes.col}>
+              <strong>{hits.title}</strong>
+              <p className={classes.desc}>{hits.desc}</p>
+              <p>
+                <a href={hits.link} className={classes.link}>
+                  See Here
+                </a>
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
