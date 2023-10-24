56272dee83dd6fb32e26de8caf85624b0c536bc3
Kevin Yuen <90118768+yuenkev@users.noreply.github.com>
2023-10-23 21:57:06 -0400

    10th Commit: Photojournalism Page -- Added the Pictorial (PPF Installation)

---
+++ src/Components/UI/Photos/PPF.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./PPF.module.css";
+import ppf1 from "../../Assets/Images/Photo/ppf1.jpg";
+import ppf2 from "../../Assets/Images/Photo/ppf2.jpg";
+import ppf3 from "../../Assets/Images/Photo/ppf3.jpg";
+
+const PPF1 = () => {
+  return (
+    <div className={classes.PPF}>
+      <h4 className={classes.title}>Pictorial (PPF Installation)</h4>
+      <div className={classes.outer}>
+        <div className={classes.column}>
+          <img src={ppf1} alt="hug" className={classes.Item} />
+          <img src={ppf2} alt="escaltors" className={classes.Item} />
+        </div>
+        <div className={classes.column}>
+          <img src={ppf3} alt="escaltors" className={classes.Item} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PPF1;
--- src/Components/UI/Photos/Pho.js
+++ src/Components/UI/Photos/Pho.js
@@ -2,12 +2,14 @@ import React from "react";
 import classes from "./Pho.module.css";
 import Butter from "./Butter";
 import Osap from "./Osap";
+import PPF from "./PPF";
 
 const Pho = () => {
   return (
     <div className={classes.Pho}>
         <Butter />
         <Osap />
+        <PPF />
     </div>
   );
 };
